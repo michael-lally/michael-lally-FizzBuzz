d8b452addd427c8c0b1ec48095f06734a59b6d5b
michael-lally <mplally@neiu.edu>
2023-02-24 22:07:06 +0000

    Completed FizzBuzz

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,15 @@ public class Lab {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
-        return "";
+        if(n%5 == 0){
+            if(n%3 == 0)
+                return "FizzBuzz";
+            else 
+                return"Buzz";
+        }
+        else if(n%3 == 0)
+            return"Fizz";
+        else
+             return "";
     }
 }
